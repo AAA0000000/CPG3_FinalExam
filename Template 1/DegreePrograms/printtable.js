e4c6c6af3b696d85bcc7e4cfca739b6eef604246
Ella-JanelleJan <8939679-Ella-JanelleJan@users.noreply.replit.com>
2023-08-04 11:37:16 +0000

    LBYCPG3_FinalExam

--- Template 1/DegreePrograms/printtable.js
+++ Template 1/DegreePrograms/printtable.js
@@ -1,100 +1,7 @@
-function newbody(x) {
-  switch (x) {
-    case 0:
-      $("#body-placeholder").load("Home/body_vision.html");
-      document.title = "DLSU DECE - Vision";
-      break;
-    case 1:
-      $("#body-placeholder").load("Home/body_mission.html");
-      break;
-    case 2:
-      $("#body-placeholder").load("Home/body_programhistory.html");
-      break;
-    case 3:
-      $("#body-placeholder").load("Home/body_elga.html");
-      break;
-    case 4:
-      $("#body-placeholder").load("Home/body_peo.html");
-      break;
-    case 5:
-      $("#body-placeholder").load("Home/body_so.html");
-      break;
-    case 10:
-      $("#body-placeholder").load("DegreePrograms/dp_ug.html");
-      break;
-    case 11:
-      $("#body-placeholder").load("DegreePrograms/dp_grad.html");
-      break;
-    case 12:
-      $("#body-placeholder").load("DegreePrograms/dp_other.html");
-      break;
-    case 13:
-      $("#body-placeholder").load("DegreePrograms/dp_cmap_ece.html");
-      break;
-    case 14:
-      $("#body-placeholder").load("DegreePrograms/dp_stats.html");
-      break;
-    case 15:
-      $("#body-placeholder").load("DegreePrograms/dp_raa.html");
-      break;
-    case 16:
-      $("#body-placeholder").load("DegreePrograms/dp_cmap_cpe.html");
-      break;
-    case 20:
-      $("#body-placeholder").load("Faculty/structure.html");
-      break;
-    case 21:
-      $("#body-placeholder").load("Faculty/profile.html");
-      break;
-    case 30:
-      $("#body-placeholder").load("CL/labs.html");
-      break;
-    case 31:
-      $("#body-placeholder").load("CL/community.html");
-      break;
-    case 32:
-      $("#body-placeholder").load("CL/student_orgs.html");
-      break;
-    case 40:
-      $("#body-placeholder").load("Ach/awards.html");
-      break;
-    case 41:
-      $("#body-placeholder").load("Ach/conferences.html");
-      break;
-    case 42:
-      $("#body-placeholder").load("Ach/licensure.html");
-      break;
-    case 50:
-      $("#body-placeholder").load("Arch/pubs.html");
-      break;
-    case 51:
-      $("#body-placeholder").load("Arch/thesis_arch.html");
-      break;
-    case 60:
-      $("#body-placeholder").load("Other/continuing.html");
-      break;
-    case 61:
-      $("#body-placeholder").load("Other/alumni.html");
-      break;
-    case 61:
-      $("#body-placeholder").load("Other/forms_policies.html");
-      break;
-    default:
-    
-      break;
-  }
-}
-
-function show(shown, hidden) {
-  document.getElementById(shown).style.display='block';
-  document.getElementById(hidden).style.display='none';
-  return false;
-}
-
-var data = [];
-var attribute;
-  function printTable(attribute, data) {
-    var table = document.getElementById(attribute);
+  var data = [];
+  var id;
+  function printTable(id, data) {
+    var table = document.getElementById(id);
     for (i = 0; i < data.length; i++) {
       var row = table.insertRow(i + 1);
       for (j = 0; j < data[i].length; j++) {
@@ -106,7 +13,7 @@ var attribute;
   function dataset(x) {
     switch (x) {
       case 1:
-        attribute = "ece_major";
+        id = "ece_major";
         data = [
           ["LBYEC2A", "Computer Fundamentals and Programming 1", 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
           ["LBYEC2B", "Computer Fundamentals and Programming 2", 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0],
@@ -163,7 +70,7 @@ var attribute;
         ];
         break;
       case 2:
-        attribute = "ece_ge";
+        id = "ece_ge";
         data = [
           ["GEARTAP", "Art Appreciation", 0, 0, 0, 0, 0, 0, 0, 0, "●", 0, 0, 0, 0],
           ["GELECAH", "Arts and Humanities", 0, 0, 0, 0, 0, 0, 0, 0, "●", 0, 0, 0, 0],
@@ -220,7 +127,7 @@ var attribute;
         ];
         break;
       case 3:
-        attribute = "cpe_major";
+        id = "cpe_major";
         data = [
         	["DISCRMT", "Discrete Mathematics", 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0], 
         	["DATSRAL", "Data Structures and Algorithms Lecture", 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0], 
@@ -279,5 +186,5 @@ var attribute;
       default:
         break;
     }
-    printTable(attribute, data);
+    printTable(id, data);
   }
